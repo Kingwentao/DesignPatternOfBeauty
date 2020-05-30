170d643b132bde256ce02a3869ee410c2c1a490e
jinwentao <jinwentao@cvte.com>
2020-05-30 08:51:21 +0800

    添加项目实战之限流框架的需求分析

---
+++ src/ProjectPractice/LimitFlowFrame/LimitFlowFrameMain.java
@@ -0,0 +1,12 @@
+package ProjectPractice.LimitFlowFrame;
+
+/**
+ * author: WentaoKing
+ * created on: 2020/5/30
+ * description: 项目实战1： 限流框架
+ * 限制每个调用方对接口请求的频率。当超过预先设定的访问频率后，我们就触发限流熔断
+ * 为了更加精细化地限流，除了限制每个调用方对公共服务平台总的接口请求频率之外，我们还希望能对单独某个接口的访问频率进行限制
+ */
+public class LimitFlowFrameMain {
+
+}
