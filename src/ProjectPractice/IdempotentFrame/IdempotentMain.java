74e4cae59136ffff6fad3b379e48ba68ada96321
jinwentao <jinwentao@cvte.com>
2020-06-13 08:43:45 +0800

    添加幂等框架的需求和分析

---
+++ src/ProjectPractice/IdempotentFrame/IdempotentMain.java
@@ -0,0 +1,12 @@
+package ProjectPractice.IdempotentFrame;
+
+/**
+ * author: WentaoKing
+ * created on: 2020/6/13
+ * description: 幂等框架
+ */
+public class IdempotentMain {
+    public static void main(String[] args) {
+
+    }
+}
